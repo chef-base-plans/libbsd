60e158572496687c37bc69f38fc5af9cdd003134
Steven Marshall <SMarshall@chef.io>
2020-06-22 16:21:29 +0100

    [libbsd] Various refactors to existing code
    Added inspec.lock to .gitignore
    Updated README.md to library format
    Added plan_name to attributes.yml
    Updates control format
    Updated inspec.yml to latest version
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/libbsd_exists.rb
+++ controls/libbsd_exists.rb
@@ -8,13 +8,20 @@ control 'core-plans-libbsd' do
   impact 1.0
   title 'Ensure libbsd libraries exist as expected'
   desc '
-  We check that the three folders that libbsd installs are present.
+  In order to test that the libraries that libbsd exist, we must first fine the package path.
+  Using the package path we ensure that the directories that it is meant to create, have been created
+    $ ls -al $PKG_PATH/include/
+      total 12
+      drwxr-xr-x 3 root root 4096 Mar  6 01:57 .
+      drwxr-xr-x 5 root root 4096 Mar  6 01:57 ..
+      drwxr-xr-x 4 root root 4096 Mar  6 01:57 bsd
   '
 
   hab_pkg_path = command("hab pkg path #{plan_ident}")
   describe hab_pkg_path do
-    its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
   end
 
   describe command("ls #{File.join(hab_pkg_path.stdout.strip, "include")}") do
